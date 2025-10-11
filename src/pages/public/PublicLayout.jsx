9aace0e7702afa8e746a7fc3b6254e8ae58e8fde
MichalArmon <armonmic22003@gmail.com>
2025-10-11 16:54:55 +0300

    C

--- src/pages/public/PublicLayout.jsx
+++ src/pages/public/PublicLayout.jsx
@@ -1,53 +1,46 @@
-// src/pages/public/PublicLayout.jsx
-import { ThemeProvider, CssBaseline, Box, Toolbar, Fab } from "@mui/material";
-import { Phone } from "@mui/icons-material";
-import { Outlet } from "react-router-dom";
-import { themeResort } from "../../theme";
+import { Button, Fab, Typography } from "@mui/material";
+import Footer from "../../components/Footer";
+import Hero from "../../components/Hero";
 import PublicNav from "./PublicNav";
+import IntroBlock from "../../components/IntroBlock";
+import TwoImageGrid from "../../components/TwoImageGrid";
+import FullScreenImage from "../../components/FullScreenImage";
+import Pblock from "../../components/Pblock";
+import TwinImages from "../../components/TwinImages";
+import { Phone } from "@mui/icons-material";
+import { Link as RouterLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { pub } from "../../../utils/publicPath";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { themeResort } from "../../theme";
 
 function PublicLayout() {
   return (
     <ThemeProvider theme={themeResort}>
       <CssBaseline />
+      <>
+        {/* כפתור שיחה צף בפינה */}
+        <Fab
+          aria-label="Call"
+          sx={{
+            position: "fixed",
+            right: 20,
+            bottom: 20,
+            bgcolor: "#fff",
+            color: "primary.main",
+            border: "2px solid",
+            borderColor: "primary.main",
+            "&:hover": { bgcolor: "#fff" },
+          }}
+        >
+          <Phone />
+        </Fab>
 
-      {/* ניווט קבוע בראש המסך */}
-      <PublicNav />
-
-      {/* Spacer בגובה ה-AppBar כדי שהתוכן לא יהיה מתחתיו */}
-      <Toolbar sx={{ minHeight: "var(--nav-h)" }} />
-
-      {/* אזור התוכן הראשי */}
-      <Box
-        component="main"
-        sx={{
-          minHeight: "calc(100dvh - var(--nav-h))",
-          px: { xs: 2, md: 4 },
-          pb: { xs: 6, md: 8 },
-        }}
-      >
-        <Outlet />
-      </Box>
-
-      {/* כפתור שיחה צף בפינה */}
-      <Fab
-        aria-label="Call"
-        component="a"
-        href="tel:+972502136623"
-        sx={{
-          position: "fixed",
-          right: 20,
-          bottom: 20,
-          zIndex: (t) => t.zIndex.tooltip + 1,
-          bgcolor: "#fff",
-          color: "primary.main",
-          border: "2px solid",
-          borderColor: "primary.main",
-          boxShadow: 2,
-          "&:hover": { bgcolor: "#fff" },
-        }}
-      >
-        <Phone />
-      </Fab>
+        <PublicNav />
+        <main className="page">
+          <Outlet />
+        </main>
+      </>
     </ThemeProvider>
   );
 }
